b51366ea40210a92643e84f24a898dbcc11da3cc
허세진 <heosejin222@gmail.com>
2026-02-15 23:46:43 +0900

    Initial commit

---
+++ src/main/java/com/flab/woowahaneats/WoowahanEatsApplication.java
@@ -0,0 +1,13 @@
+package com.flab.woowahaneats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoowahanEatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoowahanEatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flab/woowahaneats/WoowahanEatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.woowahaneats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoowahanEatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
