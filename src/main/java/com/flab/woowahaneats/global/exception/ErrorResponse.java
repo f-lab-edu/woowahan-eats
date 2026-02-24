719dc50a7b6b8b7ad09a18541cc489aab776b17f
허세진 <heosejin222@gmail.com>
2026-02-24 18:28:55 +0900

    refactor(ErrorResponse): timestamp 제거

--- src/main/java/com/flab/woowahaneats/global/exception/ErrorResponse.java
+++ src/main/java/com/flab/woowahaneats/global/exception/ErrorResponse.java
@@ -1,13 +1,10 @@
 package com.flab.woowahaneats.global.exception;
 
-import java.time.LocalDateTime;
-
 public record ErrorResponse (
         String errorCode,
-        String message,
-        LocalDateTime timestamp
+        String message
 ) {
     public static ErrorResponse of(String errorCode, String message) {
-        return new ErrorResponse(errorCode, message, LocalDateTime.now());
+        return new ErrorResponse(errorCode, message);
     }
 }
