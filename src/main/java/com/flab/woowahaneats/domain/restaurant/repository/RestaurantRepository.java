2ef5afa78ee9857e2822630baa6473c7bd064c57
허세진 <heosejin222@gmail.com>
2026-03-05 16:25:58 +0900

    fix(restaurant): Repository 어노테이션 제거

--- src/main/java/com/flab/woowahaneats/domain/restaurant/repository/RestaurantRepository.java
+++ src/main/java/com/flab/woowahaneats/domain/restaurant/repository/RestaurantRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
-@Repository
+
 public interface RestaurantRepository {
    void save(Restaurant restaurant);
    Optional<Restaurant> findById(Long id);
