1a61fbe702df64ef9facf794d7b643c24891539a
허세진 <heosejin222@gmail.com>
2026-04-07 14:24:20 +0900

    style: 서비스 인터페이스 줄바꿈 형식 통일

--- src/main/java/com/flab/woowahaneats/domain/rider/application/RiderService.java
+++ src/main/java/com/flab/woowahaneats/domain/rider/application/RiderService.java
@@ -5,8 +5,12 @@ import com.flab.woowahaneats.domain.rider.controller.dto.RiderSignUpRequest;
 public interface RiderService {
 
     void signUpRider(RiderSignUpRequest riderSignUpRequest);
+
     void startWork(Long riderId);
+
     void endWork(Long riderId);
+
     void startDelivering(Long riderId);
+
     void finishDelivering(Long riderId);
 }
