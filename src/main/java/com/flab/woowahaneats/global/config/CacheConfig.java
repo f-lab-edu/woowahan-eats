e56c1dd610dfd382bf092d30156cd1bfa26c6d16
허세진 <heosejin222@gmail.com>
2026-04-20 15:56:48 +0900

    feat: 캐시 스탬피드 방지를 위한 TTL Jitter 적용

--- src/main/java/com/flab/woowahaneats/global/config/CacheConfig.java
+++ src/main/java/com/flab/woowahaneats/global/config/CacheConfig.java
@@ -5,12 +5,14 @@ import com.flab.woowahaneats.domain.restaurant.user.controller.dto.RestaurantRes
 import java.time.Duration;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter.TtlFunction;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
@@ -20,6 +22,9 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @EnableCaching
 public class CacheConfig {
 
+    private static final long BASE_TTL_MINUTES = 30;
+    private static final long JITTER_RANGE_MINUTES = 5;
+
     @Bean
     @Primary
     public RedisCacheManager redisCacheManager(RedisConnectionFactory connectionFactory) {
@@ -28,10 +33,15 @@ public class CacheConfig {
                 new Jackson2JsonRedisSerializer<>(objectMapper,
                         objectMapper.getTypeFactory().constructCollectionType(List.class, RestaurantResponse.class));
 
+        TtlFunction jitteredTtl = (key, value) -> {
+            long jitter = ThreadLocalRandom.current().nextLong(-JITTER_RANGE_MINUTES, JITTER_RANGE_MINUTES + 1);
+            return Duration.ofMinutes(BASE_TTL_MINUTES + jitter);
+        };
+
         RedisCacheConfiguration defaultConfig = RedisCacheConfiguration.defaultCacheConfig()
                 .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
                 .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer))
-                .entryTtl(Duration.ofMinutes(30));
+                .entryTtl(jitteredTtl);
 
         return RedisCacheManager.builder(connectionFactory)
                 .cacheDefaults(defaultConfig)
