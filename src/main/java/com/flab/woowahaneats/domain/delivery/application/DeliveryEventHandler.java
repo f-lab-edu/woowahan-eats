476e99d503594a20d071d2e831c08ef9694d95ad
허세진 <heosejin222@gmail.com>
2026-05-17 18:56:41 +0900

    refactor: TransactionalEventListener 핸들러 메서드 제거
    Poller가 서비스를 직접 호출하므로 @TransactionalEventListener 핸들러가
    더 이상 필요 없음. 핸들러 메서드를 제거하고 빈 클래스만 유지.

--- src/main/java/com/flab/woowahaneats/domain/delivery/application/DeliveryEventHandler.java
+++ src/main/java/com/flab/woowahaneats/domain/delivery/application/DeliveryEventHandler.java
@@ -1,32 +1,12 @@
 package com.flab.woowahaneats.domain.delivery.application;
 
-import com.flab.woowahaneats.domain.delivery.event.DeliveryCancelledEvent;
-import com.flab.woowahaneats.domain.delivery.event.DeliveryCompletedEvent;
-import com.flab.woowahaneats.domain.delivery.event.DeliveryCreatedEvent;
 import com.flab.woowahaneats.domain.order.user.application.UserOrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.event.TransactionPhase;
-import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
 public class DeliveryEventHandler {
 
     private final UserOrderService userOrderService;
-
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    public void handleDeliveryCreated(DeliveryCreatedEvent event) {
-        userOrderService.startDelivering(event.orderId());
-    }
-
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    public void handleDeliveryCancelled(DeliveryCancelledEvent event) {
-        userOrderService.resetOrderToReady(event.orderId());
-    }
-
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    public void handleDeliveryCompleted(DeliveryCompletedEvent event) {
-        userOrderService.completeOrder(event.orderId());
-    }
 }
--- src/main/java/com/flab/woowahaneats/domain/order/user/application/UserOrderEventHandler.java
+++ src/main/java/com/flab/woowahaneats/domain/order/user/application/UserOrderEventHandler.java
@@ -1,31 +1,11 @@
 package com.flab.woowahaneats.domain.order.user.application;
 
-import com.flab.woowahaneats.domain.order.owner.event.OwnerOrderAcceptedEvent;
-import com.flab.woowahaneats.domain.order.owner.event.OwnerOrderCookingCompletedEvent;
-import com.flab.woowahaneats.domain.order.owner.event.OwnerOrderCookingStartedEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.event.TransactionPhase;
-import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
 public class UserOrderEventHandler {
 
     private final UserOrderService userOrderService;
-
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    public void handleOrderAccepted(OwnerOrderAcceptedEvent event) {
-        userOrderService.approveOrder(event.userOrderId());
-    }
-
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    public void handleCookingStarted(OwnerOrderCookingStartedEvent event) {
-        userOrderService.startCooking(event.userOrderId());
-    }
-
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    public void handleCookingCompleted(OwnerOrderCookingCompletedEvent event) {
-        userOrderService.completeCooking(event.userOrderId());
-    }
 }
